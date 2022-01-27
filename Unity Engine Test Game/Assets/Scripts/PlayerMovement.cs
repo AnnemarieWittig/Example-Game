4d4d7f3adb74456601c5efebde3fccbb7b121a64
stanwitt <annemarie.wittig@student.hs-anhalt.de>
2022-01-27 15:51:48 +0100

    hunger

--- Unity Engine Test Game/Assets/Scripts/PlayerMovement.cs
+++ Unity Engine Test Game/Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,17 @@
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
-
+    [SerializeField] float speed;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
-    private void void Update()
+    private void Update()
     {
-        
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed,body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
